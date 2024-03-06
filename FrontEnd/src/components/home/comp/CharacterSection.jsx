6d6615ac6787f321f233fa2c54337ded6fdeacd2
PierluigiMarolla <pierluigimarolla2000@gmail.com>
2024-03-06 19:32:04 +0100

    Home static almost complete

--- FrontEnd/src/components/home/comp/CharacterSection.jsx
+++ FrontEnd/src/components/home/comp/CharacterSection.jsx
@@ -1,12 +1,18 @@
-
+import plusSVG from "../../../assets/svg/plus-solid.svg";
 
 export default function CharacterSection() {
 
 
     return (
         <>
-            <div className="container-home w-2/5 bg-scroll border-2 border-ancient rounded-md opacity-70">
-
+            <div className="container-home w-2/5 bg-scroll border-2 border-ancient rounded-md bg-opacity-70">
+                <div className="flex justify-between mx-5 items-center my-2 ">
+                    <p className="text-white text-2xl cinzel">Character</p>
+                    <button>
+                        <img className="bg-scroll p-2 w-10 rounded-md border border-ancient" src={plusSVG} />
+                    </button>
+                </div>
+               
             </div>
         </>
     )
--- FrontEnd/src/components/home/comp/ChatSection.jsx
+++ FrontEnd/src/components/home/comp/ChatSection.jsx
@@ -1,12 +1,57 @@
-
+import chatLogo from "../../../assets/img/chatLogoPlaceHolder.png"
 
 export default function ChatSection() {
     
 
     return(
         <>
-            <div className="container-home w-2/5 bg-scroll border-2 border-ancient rounded-md opacity-70">
-
+            <div className="container-home w-2/5 bg-scroll border-2 border-ancient rounded-md bg-opacity-70 overflow-y-scroll">
+                <div className="flex justify-between mx-5 my-2 items-center">
+                    <p className="text-white text-2xl cinzel">Chats</p>
+                </div>
+                <div className="border-y">
+                    <div className="w-full bg-ancient py-3 flex justify-between items-center">
+                        <img className="w-24" src={chatLogo}/>
+                        <div className="text-center">
+                            <p className="cinzel">Chat Title</p>
+                            <p className="medievalsharp">Chat Last Message</p>
+                        </div>
+                        <div className="text-center">
+                            <p className="cinzel">Chat Members</p>
+                            <p className="medievalsharp">4/10</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="border-b">
+                    <div className="w-full bg-ancient py-3">
+                        <img className="w-24" src={chatLogo}/>
+                    </div>
+                </div>
+                <div className="border-b">
+                    <div className="w-full bg-ancient py-3">
+                        <img className="w-24" src={chatLogo}/>
+                    </div>
+                </div>
+                <div className="border-b">
+                    <div className="w-full bg-ancient py-3">
+                        <img className="w-24" src={chatLogo}/>
+                    </div>
+                </div>
+                <div className="border-b">
+                    <div className="w-full bg-ancient py-3">
+                        <img className="w-24" src={chatLogo}/>
+                    </div>
+                </div>
+                <div className="border-b">
+                    <div className="w-full bg-ancient py-3">
+                        <img className="w-24" src={chatLogo}/>
+                    </div>
+                </div>
+                <div className="border-b">
+                    <div className="w-full bg-ancient py-3">
+                        <img className="w-24" src={chatLogo}/>
+                    </div>
+                </div>
             </div>
         </>
     )
